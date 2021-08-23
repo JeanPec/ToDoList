a4c0a0a50eebae2ff1ef2958c38578ea2d491ebc
JeanPec <andremarie67450@gmail.com>
2021-08-23 12:06:01 +0200

    Gave up too many features to update tutorial not up to date anymore

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -3,5 +3,25 @@ pragma solidity ^0.5.0;
 contract ToDoList {
     uint public taskCount = 0;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
 
+    mapping(uint=>Task) public tasks;
+
+    constructor() public{
+        createTask("Buy Ether");
+        createTask("Sell Ether");
+        createTask("Loose Money");
+        createTask("Be Bilionnaire");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount]= Task(taskCount,_content,false);
+    }
+
+ 
 }
